a6b272a698c9ca5d756e1f97a7a8919a52947b28
Rendy Arya Kemal <renrror@gmail.com>
2022-01-28 21:19:33 +0700

    Disable devtool

--- preact.config.js
+++ preact.config.js
@@ -1,6 +1,7 @@
 const tailwind = require('preact-cli-tailwind');
 
 module.exports = (config, env, helpers) => {
+  config.devtool = false;
   config = tailwind(config, env, helpers);
   return config;
 };
